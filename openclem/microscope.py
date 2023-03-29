00a6c3d3f6303701513be99190b29df5c3af13e2
DavidDierickx <David.Dierickx1@monash.edu>
2023-03-29 13:16:15 +1100

    small microscope abstract class update

--- openclem/microscope.py
+++ openclem/microscope.py
@@ -1,16 +1,24 @@
 from abc import ABC, abstractmethod
 from openclem.structures import ImageSettings
+from openclem.detector import Detector
+from openclem.laser import LaserController, Laser
 
 class LightMicroscope(ABC):
     """Abstract class for light microscope"""
+    def __init__(self, detector: Detector, laser_controller: LaserController, laser: Laser):
+        self.detector = detector
+        self.laser_controller = laser_controller
+        self.laser = laser
 
     @abstractmethod
     def connect_to_microscope(self):
-        pass
+        self.detector.connect()
+        self.laser_controller.connect()
     
     @abstractmethod
     def disconnect(self):
-        pass
+        self.detector.disconnect()
+        self.laser_controller.disconnect()
 
     # @abstractmethod
     # def acquire_image(self, image_settings:ImageSettings):
