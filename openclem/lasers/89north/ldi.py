2b50468e4d2448e281eb930ec204e9df736d3ce1
DavidDierickx <David.Dierickx1@monash.edu>
2023-03-29 13:03:31 +1100

    try-except removal and filling of basler, iCLE

--- openclem/lasers/89north/ldi.py
+++ openclem/lasers/89north/ldi.py
@@ -6,6 +6,51 @@ from openclem import utils
 from openclem.laser import Laser, LaserController
 from openclem.structures import LaserControllerSettings, LaserSettings
 
+class LdiLaserController(LaserController):
+    def __init__(self, laser_controller_settings: LaserControllerSettings):
+        self.settings = laser_controller_settings
+        self._name = self.settings.name
+        self._laser = self.settings.laser
+        self.serial_connection = None
+        self.lasers = {}
+
+    @classmethod
+    def __id__(self):
+        return "ldi"
+
+    @property
+    def name(self):
+        return self._name
+
+    def add_laser(self, laser: Laser):
+        self.lasers[laser.name] = laser
+
+    def connect(self):
+        self.serial_connection = utils.connect_to_serial_port(serial_settings=self.settings.serial_settings)
+
+    def disconnect(self):
+        if self.serial_connection is None: return
+        self.serial_connection.close()
+        self.serial_connection = None
+
+    def get_laser(self, name: str) -> Laser:
+        return self.lasers[name]
+
+    def set_power(self, name: str, value: float) -> None:
+        self.lasers[name].power = value
+
+    def get_power(self, name: str) -> float:
+        return self.lasers[name].power
+
+    # TODO: determine whether there is a salient difference between idling/shutter closing
+    def close_emission(self):
+        for laser in self.lasers.values():
+            laser.emission_off()
+
+    def close_shutters(self):
+        for laser in self.lasers.values():
+            laser.disable()
+
 
 class LdiLaser(Laser):
     def __init__(self, laser_settings: LaserSettings, parent: LaserController):
@@ -107,49 +152,4 @@ class LdiLaser(Laser):
 
     def check_response(self, response):
         return response == b'ok\n' or response == b'ok\r' or response == b'ok\r\n'
-
-class LdiLaserController(LaserController):
-    def __init__(self, laser_controller_settings: LaserControllerSettings):
-        self.settings = laser_controller_settings
-        self._name = self.settings.name
-        self._laser = self.settings.laser
-        self.serial_connection = None
-        self.lasers = {}
-
-    @classmethod
-    def __id__(self):
-        return "ldi"
-
-    @property
-    def name(self):
-        return self._name
-
-    def add_laser(self, laser: Laser):
-        self.lasers[laser.name] = laser
-
-    def connect(self):
-        self.serial_connection = utils.connect_to_serial_port(serial_settings=self.settings.serial_settings)
-
-    def disconnect(self):
-        if self.serial_connection is None: return
-        self.serial_connection.close()
-        self.serial_connection = None
-
-    def get_laser(self, name: str) -> Laser:
-        return self.lasers[name]
-
-    def set_power(self, name: str, value: float) -> None:
-        self.lasers[name].power = value
-
-    def get_power(self, name: str) -> float:
-        return self.lasers[name].power
-
-    # TODO: determine whether there is a salient difference between idling/shutter closing
-    def close_emission(self):
-        for laser in self.lasers.values():
-            laser.emission_off()
-
-    def close_shutters(self):
-        for laser in self.lasers.values():
-            laser.disable()
 
--- openclem/lasers/toptica/iCLE.py
+++ openclem/lasers/toptica/iCLE.py
@@ -1,29 +1,106 @@
 from openclem.laser import LaserController, Laser
 from openclem import utils
 import numpy as np
+from openclem.structures import LaserControllerSettings, LaserSettings
+
+
 
 class iCLELaserController(LaserController):
-    def __init__(self, wavelength):
-        self._wavelength = wavelength
-        self._power = 0.0
-        self.serial_id = None
-
+    def __init__(self, laser_controller_settings: LaserControllerSettings):
+        self.settings = laser_controller_settings
+        self._name = self.settings.name
+        self._laser = self.settings.laser
+        self.serial_connection = None
+        self.lasers = {}
+    
     @classmethod
     def __id__(self):
         return "iCLE"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    def add_laser(self, laser: Laser):
+        self.lasers[laser.name] = laser
+
+    def connect(self):
+        self.serial_connection = utils.connect_to_serial_port(serial_settings=self.settings.serial_settings)
+
+    def disconnect(self):
+        if self.serial_connection is None: return
+        self.serial_connection.close()
+        self.serial_connection = None
+
+    def get_laser(self, name: str) -> Laser:
+        return self.lasers[name]
+
+    def set_power(self, name: str, value: float) -> None:
+        self.lasers[name].power = value
+
+    def get_power(self, name: str) -> float:
+        return self.lasers[name].power
+
+    # TODO: determine whether there is a salient difference between idling/shutter closing
+    def close_emission(self):
+        for laser in self.lasers.values():
+            laser.emission_off()
+
+    def close_shutters(self):
+        for laser in self.lasers.values():
+            laser.disable()
+
+class iCLELaser(Laser):
+    def __init__(self, laser_settings: LaserSettings, parent: LaserController):
+        self.settings = laser_settings
+        self._name = laser_settings.name
+        self._serial_id = laser_settings.serial_id
+        self._wavelength = laser_settings.wavelength
+        self._power = laser_settings.power
+        self._exposure_time = laser_settings.exposure_time
+        self._enabled = laser_settings.enabled
+        self._parent = parent
+        if self._enabled:
+            self.enable()
+        else:
+            self.disable()
+        self._colour = laser_settings.colour
+
+    @classmethod
+    def __id__(self):
+        return "iCLElaser"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def serial_id(self):
+        return self._serial_id
+
+    @serial_id.setter
+    def serial_id(self, value):
+        self._serial_id = value
 
     @property
     def power(self):
         return self._power
-
+    
     @power.setter
     def power(self, value):
-        value = np.clip(value, 0.0, 100.0)
-        command = (
-            "(param-set! '" + self.serial_id + ":level " + str(round(value, 2)) + ")\r"
-        )
-        utils.write_serial_command(self.serial_connection, command)
-        self._power = value
+        pass
+
+    @property
+    def wavelength(self):
+        return self._wavelength
+    
+    @wavelength.setter
+    def wavelength(self, value):
+        pass
 
     def emission_on(self):
         print('Emission on')
@@ -37,7 +114,3 @@ class iCLELaserController(LaserController):
     def disable(self):
         print('Disable')
 
-class iCLELaser(Laser):
-    @classmethod
-    def __id__(self):
-        return "iCLElaser"
