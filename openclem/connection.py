bef9fcf12eeafcbe8c6ba2e77e66d267d38e4232
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-03-29 15:24:40 +1100

    mv serial to connections.py

--- openclem/connection.py
+++ openclem/connection.py
@@ -1,7 +1,32 @@
 # connections
 
-
-
 # serial (usb)
+import serial
+
+def write_serial_command(port: serial.Serial, command):
+    port.close()
+    port.open()
+    port.write(bytes(command, "utf-8"))
+    response = port.read_until(expected=b"\r")
+    port.close()
+    return response
+
+
+def get_available_ports():
+    ports = serial.tools.list_ports.comports()
+    return ports
+
+
+def get_port_names(ports):
+    port_names = [port.device for port in ports]
+    return port_names
+
+
+def connect_to_serial_port(port, baudrate=9600, timeout=1):
+
+    serial_connection = serial.Serial(
+        port=port, baudrate=baudrate, timeout=timeout
+    )
+    return serial_connection
 
 # socket (tcp/ip)
