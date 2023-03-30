4bf6a089a15c3bfcd69c03e40e351e355475b49c
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-03-30 19:10:16 +1100

    add demo synchroniser

---
+++ openclem/synchronisers/demo/demo.py
@@ -0,0 +1,43 @@
+from openclem import utils
+from openclem.structures import SynchroniserMessage, SynchroniserSettings
+from openclem.synchronisation import Synchroniser
+import time
+import logging
+
+MODE_CONVERSION = {
+    "single": "S",
+    "live": "L",
+    "volume": "V",
+}
+
+class DemoSynchroniser(Synchroniser):
+    def __init__(self, synchroniser_settings: SynchroniserSettings):
+        self.settings = synchroniser_settings
+        self.name = synchroniser_settings.name
+        self.pins = {}
+        self.serial_connection = None
+
+    def connect(self):
+        self.serial_connection = "Serial Connection"
+
+    def disconnect(self):
+        if self.serial_connection is not None:
+            logging.info(f"Closing serial connection {self.serial_connection}")
+
+    def send_command(self, command):
+        if self.serial_connection is None: return
+
+        logging.info(f"Sending command {command}")
+        return "Response"
+
+    def sync_image(self, message: SynchroniserMessage):
+        exposure_string = ""
+        exposures = message.exposures
+        edge = message.trigger_edge
+        for exposure in exposures:
+            exposure_string += f"{exposure} "
+        n_slices = message.n_slices
+        mode = MODE_CONVERSION[message.mode]
+        command = f"E{mode}{exposure_string}{n_slices} {edge.value}"
+        self.send_command(command)
+        return command
