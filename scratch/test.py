434de1a8f9879b98519774f338731f2c431ef46e
Daviddierickx <David.Dierickx1@monash.edu>
2023-03-31 09:25:17 +1100

    create piedisc yaml file

--- scratch/test.py
+++ scratch/test.py
@@ -4,7 +4,8 @@ from openclem import config
 from openclem.structures import SynchroniserMessage, ImageSettings, DetectorSettings
 import time
 
-cfg_path = os.path.join(config.BASE_PATH, "config", "system.yaml")
+# cfg_path = os.path.join(config.BASE_PATH, "config", "system.yaml")
+cfg_path = os.path.join(config.BASE_PATH, "config", "piedisc.yaml")
 cfg = utils.load_yaml(cfg_path)
 microscope, settings = utils.setup_session(config_path=cfg_path)
 
